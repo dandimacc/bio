0d03c50e228d13433df6ac64e7fb6716ff4d4073
Dandi Muhammad Alim <91006062+dandimacc@users.noreply.github.com>
2024-05-18 06:04:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Business Executive", "Senior Key Account Manager"],
+        strings: ["Business Executive", "Trade Marketing", "Key Account Manager"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Business Executive", "Senior Key Account Manager"],
+        strings: ["Business Executive", "Trade Marketing", "Key Account Manager"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
